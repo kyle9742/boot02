eb691fdf49dd88636b95344c6d13588e55a1cd0d
it <kyle9742@naver.com>
2024-07-15 12:49:51 +0900

    이미지 업로드, 수정, 삭제

---
+++ src/main/java/zerock/boot02/dto/UploadFileDTO.java
@@ -0,0 +1,11 @@
+package zerock.boot02.dto;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+public class UploadFileDTO {
+    private List<MultipartFile> files;
+}
